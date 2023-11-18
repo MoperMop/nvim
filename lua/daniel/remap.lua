b66e681dcf68d07dab2fc2da528003cc5414a0f2
Daniel Parker <dantiptoe@gmail.com>
2023-11-18 10:09:27 -0700

    added keymaps for moving between panels

--- lua/daniel/remap.lua
+++ lua/daniel/remap.lua
@@ -16,6 +16,11 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+vim.keymap.set("n", "<A-h>", "<C-w>h")
+vim.keymap.set("n", "<A-l>", "<C-w>l")
+vim.keymap.set("n", "<A-j>", "<C-w>j")
+vim.keymap.set("n", "<A-k>", "<C-w>k")
+
 
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
