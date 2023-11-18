12125feeed9930cb1048e94ea5e123d7cc588802
Daniel Parker <dantiptoe@gmail.com>
2023-11-18 11:30:44 -0700

    added mason.nvim

--- lua/daniel/plugins.lua
+++ lua/daniel/plugins.lua
@@ -19,7 +19,7 @@ if not status_ok then
 end
 
 lazy.setup({
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}, 
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.4',
     dependencies = { 'nvim-lua/plenary.nvim' }
@@ -27,4 +27,8 @@ lazy.setup({
   {"ThePrimeagen/harpoon", dependencies = { "nvim-lua/plenary.nvim" }},
   "mbbill/undotree",
   "tpope/vim-fugitive",
+
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  "neovim/nvim-lspconfig",
 })
