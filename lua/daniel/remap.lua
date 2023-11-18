beba7147bd2dc06f8612c8fdb5abbae135e3f420
Daniel Parker <dantiptoe@gmail.com>
2023-11-17 20:13:39 -0700

    added plugin manager

--- lua/daniel/remap.lua
+++ lua/daniel/remap.lua
@@ -24,6 +24,11 @@ vim.keymap.set("n", "<leader>Y", "\"+Y")
 vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
 
+vim.keymap.set("n", "<leader>p", "\"+p")
+vim.keymap.set("v", "<leader>p", "\"+p")
+vim.keymap.set("n", "<leader>P", "\"+P")
+vim.keymap.set("v", "<leader>P", "\"+P")
+
 
 vim.keymap.set("n", "<leader>s",
   ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
