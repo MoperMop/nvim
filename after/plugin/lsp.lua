14efa0c7ec707a3167cdcd4f05f57c23162fb6a8
Daniel Parker <dantiptoe@gmail.com>
2023-11-19 13:22:19 -0700

    added more cmp sources

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -52,10 +52,26 @@ cmp.setup({
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
     { name = "luasnip" },
+  }, {
+    { name = "buffer" },
   }),
   snippet = {
     expand = function(args)
       luasnip.lsp_expand(args.body)
     end,
   },
+})
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' },
+  }
+})
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
 })
--- lua/daniel/plugins.lua
+++ lua/daniel/plugins.lua
@@ -40,8 +40,12 @@ lazy.setup({
   "williamboman/mason-lspconfig.nvim",
   "neovim/nvim-lspconfig",
 
+	'hrsh7th/cmp-buffer',
+	'hrsh7th/cmp-path',
+	'hrsh7th/cmp-cmdline',
   "hrsh7th/nvim-cmp",
   "hrsh7th/cmp-nvim-lsp",
+  'saadparwaiz1/cmp_luasnip',
   "L3MON4D3/LuaSnip",
 
 
