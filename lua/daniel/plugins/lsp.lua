f878daf7318e5eb03ab5af73bfe048a98b4570f3
Daniel Parker <dantiptoe@gmail.com>
2023-12-02 11:47:44 -0700

    changed mason's ensure_installed setup

--- lua/daniel/plugins/lsp.lua
+++ lua/daniel/plugins/lsp.lua
@@ -4,8 +4,10 @@ local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 require("mason-lspconfig").setup({
   ensure_installed = {
     "tsserver",
-    "eslint",
     "lua_ls",
+    "cssls",
+    "html",
+    "emmet_language_server",
   },
   handlers = {
     function(server)
